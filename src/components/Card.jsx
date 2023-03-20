0dce9bc18e14fdf454e75c7b69420914aea3bf2b
Josh Smith <jmsmith1018@gmail.com>
2023-03-20 10:50:16 -0600

    feat: build score tracking into game

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
-export default function Card({ villager }) {
+export default function Card({ villager, handler }) {
   const name = villager.name["name-USen"];
 
   return (
-    <div className={`h-[275px] w-[250px] rounded-lg border-2 border-black p-2`}>
+    <div
+      className="h-[275px] w-[250px] cursor-pointer rounded-lg border-2 border-black p-2"
+      onClick={handler}
+    >
       <img src={villager.image_uri} className="rounded-lg" />
       <p>{name}</p>
     </div>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,15 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import callVillagerApi from "../services/animal_crossing_api";
 
 import Card from "./Card";
 
 export default function Game() {
-  const [score] = useState(0);
+  const [score, setScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
   const [cards, setCards] = useState(undefined);
+  const usedCards = useRef(new Set());
 
   useEffect(() => {
     callVillagerApi().then((result) => setCards(result));
   }, []);
+
+  useEffect(() => {
+    setMaxScore(Math.max(score, maxScore));
+  }, [score]);
+
+  const addPoint = (id) => {
+    return () => {
+      if (usedCards.current.has(id)) {
+        setScore(0);
+        usedCards.current = new Set();
+      } else {
+        usedCards.current.add(id);
+        setScore(score + 1);
+      }
+    };
+  };
 
   const randomOrder = () => {
     return cards
@@ -25,12 +43,17 @@ export default function Game() {
           Earn points by clicking on a character, but don't click on any more
           than once!
         </p>
-        <p>Score: {score}</p>
+        <div>
+          <p>Score: {score}</p>
+          <p>Best Score: {maxScore}</p>
+        </div>
       </div>
       <div className="flex flex-wrap justify-center gap-6">
         {cards &&
           randomOrder().map((card) => {
-            return <Card key={card.id} villager={card} />;
+            return (
+              <Card key={card.id} villager={card} handler={addPoint(card.id)} />
+            );
           })}
       </div>
     </div>
